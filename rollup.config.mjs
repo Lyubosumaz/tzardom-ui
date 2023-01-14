c17928adf2d16d451d22658c9798deff265147f8
Lyubosumaz <lyuboslavilkov@gmail.com>
2023-01-14 18:21:37 +0200

    optimizing build bundle via plugin-terser

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,6 +3,8 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 import postcss from 'rollup-plugin-postcss';
+import terser from '@rollup/plugin-terser';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 import packageJson from './package.json' assert { type: 'json' };
 
@@ -21,9 +23,16 @@ export default [
                 sourcemap: true,
             },
         ],
-        plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' }),
-        postcss()
-    ],
+        plugins: [
+            peerDepsExternal(),
+            resolve(),
+            commonjs(),
+            typescript({ tsconfig: './tsconfig.json' }),
+            postcss({
+                plugins: [],
+            }),
+            terser(),
+        ],
     },
     {
         input: 'dist/esm/types/index.d.ts',
